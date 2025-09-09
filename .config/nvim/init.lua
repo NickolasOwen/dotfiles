62836aab5a982f9d2b5f67faf911b93acbddfc95
Nick Owen <nickolas.owen@protonmail.com>
2025-09-08 23:28:17 -0500

    updated

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,13 +68,7 @@ local plugins = {
     end
   },
   -- Color Themes
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-  { "scottmckendry/cyberdream.nvim", name = "cyberdream", lazy = false, priority = 1000 },
-  { "folke/tokyonight.nvim", name = "tokyonight", lazy = false, priority = 1000 },
   { "sainnhe/sonokai", name = "sonokai", lazy = false, priority = 1000 },
-  { "HoNamDuong/hybrid.nvim", name = "hybrid", lazy = false, priority = 1000 },
-  --
   --
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "williamboman/mason.nvim",
@@ -97,11 +91,6 @@ local plugins = {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
   },
   -- Astro
   {
@@ -140,185 +129,73 @@ local plugins = {
     end
   },
   { "terrortylor/nvim-comment" },
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
+
+  {  -- Autocomplete
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      signature = { enabled = true },
+      keymap = { preset = 'default' },
+
+      appearance = {
+        nerd_font_variant = 'mono'
       },
-      'saadparwaiz1/cmp_luasnip',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
+
+      completion = { documentation = { auto_show = true } },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" }
     },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
-        sources = {
-          {
-            name = 'lazydev',
-            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-            group_index = 0,
-          },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
+    opts_extend = { "sources.default" }
   },
 }
+
+-- vim.pack.add{
+--   { src = 'https://github.com/neovim/nvim-lspconfig' },
+-- }
+
 local opts = {}
 -- Initialize plugins
 require("lazy").setup(plugins, opts)
--- require("catppuccin").setup()
 require("mason").setup()
-require("mason-lspconfig").setup{
-  ensure_installed = { "html", "lua_ls", "rust_analyzer", "bashls", "yamlls", "marksman", "powershell_es", "cssls", "astro"},
-  automatic_installation = true,
-}
+
+vim.lsp.enable({ "html", "lua_ls", "rust_analyzer", "bashls", "yamlls", "marksman", "powershell_es", "cssls", "astro", "vue_ls", "vtsls" })
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+})
+
+vim.lsp.config("vtsls", {
+  filetypes = { "typescript", "javascript", "vue" }
+})
+
 require("fidget").setup()
 require('nvim_comment').setup()
--- require('todo_comment').setup()
-
--- LSP
-local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-    },
-  },
-}
-local util = require "lspconfig/util"
-
-lspconfig.rust_analyzer.setup({
-  filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-    ['rust_analyzer'] = {
-      cargo = {
-        allFeatures = true,
-      },
-    },
-  },
-})
-lspconfig.powershell_es.setup {}
-lspconfig.yamlls.setup {}
-lspconfig.marksman.setup {}
-lspconfig.html.setup {}
-lspconfig.cssls.setup {}
--- lspconfig.volar.setup {}
--- lspconfig.ts_ls.setup {}
-
--- local lspconfig = require('lspconfig')
-
--- lspconfig.volar.setup {
---   -- add filetypes for typescript, javascript and vue
---   filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
---   init_options = {
---     vue = {
---       -- disable hybrid mode
---       hybridMode = false,
---     },
---   },
--- }
 
 -- Set colorscheme
--- vim.cmd.colorscheme "catppuccin"
--- vim.cmd.colorscheme "moonfly"
--- vim.cmd.colorscheme "tokyonight"
--- vim.cmd.colorscheme "cyberdream"
 vim.cmd.colorscheme "sonokai"
--- vim.cmd.colorscheme "hybrid"
 
 vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 
 -- Treesitter
 local configs = require("nvim-treesitter.configs")
 configs.setup({
-  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "css", "yaml", "markdown", "rust", "astro" },
+  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "css", "yaml", "markdown", "rust", "astro", "vue", "typescript" },
   sync_install = false,
   highlight = { enable = true },
   indent = { enable = true },
